b731875dbb29d385eb3db1e359d8e75fe56844f4
Rebecca Valentine <psygnisfive@yahoo.com>
2018-05-06 01:30:30 -0700

    fixes newline bug

--- uploader_app.py
+++ uploader_app.py
@@ -63,13 +63,13 @@ def title_of_processed_video(title, author):
     return "Noisebytes - " + title + " by " + author
 
 def description_of_video(title, author):
-    return "Title: " + title + "\n" +
-           "Author: " + author + "\n" +
-           "Patreon: https://patreon.com/noisebridge\n" +
-           "Website: https://noisebridge.net/\n\n" +
-           "Noisebridge is a 501(c)3 non-profit hackerspace in San Francisco's Mission District, located at 2169 Mission St.\n\n"+
-           "We're open to the public every day from 11am to 10pm, so feel free to drop by!\n\n" +
-           "We're also funded entirely by the community, so please donate if you like what we do, or the content we produce.\n\n" +
+    return "Title: " + title + "\n" +\
+           "Author: " + author + "\n" +\
+           "Patreon: https://patreon.com/noisebridge\n" +\
+           "Website: https://noisebridge.net/\n\n" +\
+           "Noisebridge is a 501(c)3 non-profit hackerspace in San Francisco's Mission District, located at 2169 Mission St.\n\n"+\
+           "We're open to the public every day from 11am to 10pm, so feel free to drop by!\n\n" +\
+           "We're also funded entirely by the community, so please donate if you like what we do, or the content we produce.\n\n" +\
            "Noisebridge is a safe space. You can read about what that means to us at the pages listed here: https://www.noisebridge.net/wiki/Safe_Space"
 
 def random_name():
